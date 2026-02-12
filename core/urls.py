4fc5a45240030a4d78dddda13161b5741b1ccdb1
JesusMedinaGomez <jesus.emg01@gmail.com>
2026-02-12 17:32:13 -0600

    media correction

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,17 @@
-# core/urls.py
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from apps.pages import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # ✅ PRIMERO EL ADMIN (debe ir antes del catch-all)
     path('admin/', admin.site.urls),
-    
-    # ✅ TUS URLs DE LA APP
     path('', include('apps.pages.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.views.static import serve
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
